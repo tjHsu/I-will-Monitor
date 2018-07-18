2950c404add890560dd7ac4ca60932f9dd830f1f
tjHsu <tim800821@hotmail.com>
2018-07-18 18:59:52 +0200

    attempt

--- server/routes/stats.js
+++ server/routes/stats.js
@@ -54,103 +54,128 @@ router.get('/search/', (req, res, next) => {
 
   console.log("DEBUG I want to see query:", req.query)
   var geocode = "";
-  // serviceGeocode.get(`${req.query.location}`)
-    // .then(response => {
-    //   // let bounds = response.data.results[0].geometry
-    //   console.log(response.data.results[0])
-    //   let lat = response.data.results[0].geometry.location.lat
-    //   let lng = response.data.results[0].geometry.location.lng
-    //   let upperRightLat = parseFloat(response.data.results[0].geometry.bounds.northeast.lat)
-    //   let upperRightLng = parseFloat(response.data.results[0].geometry.bounds.northeast.lng)
-    //   let bottomLeftLat = parseFloat(response.data.results[0].geometry.bounds.southwest.lat)
-    //   let bottomLeftLng = parseFloat(response.data.results[0].geometry.bounds.southwest.lng)
-    //   let radius = 0.5 * Math.sqrt(((upperRightLat - bottomLeftLat) * 110.574) ** 2 + ((upperRightLng - bottomLeftLng) * Math.cos((upperRightLat - bottomLeftLat) / 2 / 180 * Math.PI) * 111.320) ** 2)
-    //   geocode = lat + "," + lng + "," + radius + "km";
-    //   console.log("I see geocode:", geocode)
-    //   return geocode
-    // })
-    // .then(
-
-      // Stat.find({ keyword: {$in : [ `${req.query.keyword.toUpperCase()}`]}, location: `${req.query.location ? req.query.location.toUpperCase() : ""}` })
-      let tempArr=req.query.keyword.toUpperCase().split(',')
-      let tempArrLocation=req.query.location.toUpperCase().split(',')
-      console.log("DEBUG tempArr", tempArr)
-      let query = { keyword: {$in : tempArr}, 
-                    location: {$in: tempArrLocation} 
-                  }
-      Stat.find(query)
+  let tempArr = req.query.keyword.toUpperCase().split(',')
+  let tempArrLocation = req.query.location.toUpperCase().split(',')
+  console.log("DEBUG tempArr", tempArr)
+  console.log("DEBUG locationArr", tempArrLocation)
+  let delayCounter = 1000;
+  for (let i = 0; i < tempArr.length; i++) {
+    for (let j = 0; j < tempArrLocation.length; j++) {
+      if (tempArr[i] === "" || tempArrLocation[j] === "") {
+        continue
+      }
       
+      let query = {
+        keyword: tempArr[i],
+        location: tempArrLocation[j]
+      }
+      Stat.find(query)
         .then(stats => {
-          // console.log(`find query: ${req.query.keywords.toUpperCase()} and ${req.query.location ? req.query.location.toUpperCase() : ""}`)
-          console.log(`keyword: {$in : [ ${req.query.keyword.toUpperCase()}]}, location: {$in : [${req.query.location.toUpperCase()}]} `)
-
-          
-
-          console.log("Stat: ", stats)
-          if (stats.length != 0) {
-
-            res.json(stats);
-            // throw getBreackChainError();
-          } else if (stats.length == 0 && !req.query.location) {
-            Stat.find({ keyword: `${req.query.keyword.toUpperCase()}` })
-              .then(stats => {
-                console.log(`find query: ${req.query.keyword.toUpperCase()}`)
-                console.log("Stat: ", stats)
-                if (stats.length != 0) {
-                  res.json(stats);
-                  // throw getBreackChainError();
-                } else if (stats.length == 0 && !req.query.keyword) {
-                  Stat.find()
-                    .then(stats => {
-                      console.log(`find query: none`)
-                      console.log("Stat: ", stats)
-                      if (stats.length != 0) {
-                        res.json(stats);
-                        // throw getBreackChainError();
+          if (stats.length == 0) {
+            delayCounter+=1000;
+            console.log("Query in forloop:", query)
+            serviceGeocode.get(`${query.location}`)
+              .then(response => {
+                // let bounds = response.data.results[0].geometry
+                console.log("in forloop after geocode", response.data.results[0])
+                let lat = response.data.results[0].geometry.location.lat
+                let lng = response.data.results[0].geometry.location.lng
+                let upperRightLat = parseFloat(response.data.results[0].geometry.bounds.northeast.lat)
+                let upperRightLng = parseFloat(response.data.results[0].geometry.bounds.northeast.lng)
+                let bottomLeftLat = parseFloat(response.data.results[0].geometry.bounds.southwest.lat)
+                let bottomLeftLng = parseFloat(response.data.results[0].geometry.bounds.southwest.lng)
+                let radius = 0.5 * Math.sqrt(((upperRightLat - bottomLeftLat) * 110.574) ** 2 + ((upperRightLng - bottomLeftLng) * Math.cos((upperRightLat - bottomLeftLat) / 2 / 180 * Math.PI) * 111.320) ** 2)
+                geocode = lat + "," + lng + "," + radius + "km";
+                console.log("in forloop I see geocode:", geocode)
+                return geocode
+              })
+              .then(() => {
+                setTimeout(function () {
+                  let term = query.keyword;
+                  console.log("Before search tweet check query:", term, " geocode:", geocode, "||end")
+                  client.get('search/tweets', { q: `${term}`, geocode: `${geocode}`, count: 100 }, function (error, tweets, response) {
+                    // '52.50206,13.40701,30km'
+                    // '48.85994, 2.34146, 30km'
+                    // '45.47625,9.18302, 30km'
+                    // console.log(tweets);
+                    console.log("DEBUG: Got tweet");
+                    let createArr = tweets.statuses.map(x => x.created_at)
+                    let today = new Date();
+                    console.log("DEBUG: today", today)
+                    let filterArr = createArr.filter(function (s) {
+                      let temp = new Date(s);
+                      if (today - temp < 86400000) {
+                        return true
+                      } else {
+                        return false
                       }
                     })
-                  }
-
-
+                    let obj = {}
+                    obj["keyword"] = `${term}`
+                    obj["location"] = `${query.location}`;
+                    obj["count"] = filterArr.length;
+                    Stat.create(obj);
+                    let arr = [];
+                    arr.push(obj)
+                    console.log(arr)
+                    // res.json(arr)
+                  })
+                }, delayCounter);
               })
-          }              else {
-            setTimeout(function () {
-              console.log("Before search tweet check query:", term, " geocode:", geocode, "||end")
-              client.get('search/tweets', { q: `${term}`, geocode: `${geocode}`, count: 100 }, function (error, tweets, response) {
-                // '52.50206,13.40701,30km'
-                // '48.85994, 2.34146, 30km'
-                // '45.47625,9.18302, 30km'
-                // console.log(tweets);
-                console.log("DEBUG: Got tweet");
-                let createArr = tweets.statuses.map(x => x.created_at)
-                let today = new Date();
-                console.log("DEBUG: today", today)
-                let filterArr = createArr.filter(function (s) {
-                  let temp = new Date(s);
-                  if (today - temp < 86400000) {
-                    return true
-                  } else {
-                    return false
-                  }
-                })
-                let obj = {}
-                obj["keyword"] = `${term}`
-                obj["location"] = `${req.query.location.toUpperCase()}`;
-                obj["count"] = filterArr.length;
-                Stat.create(obj);
-                let arr = [];
-                arr.push(obj)
-                console.log(arr)
-                res.json(arr)
-              })
-            }, 2500);
-            // .catch(err => next(err))
 
 
           }
-
         })
-    // )
+    }
+  }
+
+
+  let query = {
+    keyword: { $in: tempArr },
+    location: { $in: tempArrLocation }
+  }
+  setTimeout(function () {
+  Stat.find(query)
+
+    .then(stats => {
+      // console.log(`find query: ${req.query.keywords.toUpperCase()} and ${req.query.location ? req.query.location.toUpperCase() : ""}`)
+      console.log(`keyword: {$in : [ ${req.query.keyword.toUpperCase()}]}, location: {$in : [${req.query.location.toUpperCase()}]} `)
+
+
+
+      console.log("first Stat: ", stats)
+      if (stats.length != 0) {
+
+        res.json(stats);
+        // throw getBreackChainError();
+      } else if (stats.length == 0 && !req.query.location) {
+        Stat.find({ keyword: `${req.query.keyword.toUpperCase()}` })
+          .then(stats => {
+            console.log(`find query: ${req.query.keyword.toUpperCase()}`)
+            console.log("second Stat: ", stats)
+            if (stats.length != 0) {
+              console.log("second Stat output: ")
+              res.json(stats);
+              // throw getBreackChainError();
+            } else if (stats.length == 0 ) {
+              Stat.find()
+                .then(stats => {
+                  console.log(`find query: none`)
+                  console.log("third Stat: ", stats)
+                  if (stats.length != 0) {
+                    res.json(stats);
+                    // throw getBreackChainError();
+                  }
+                })
+            }
+
+
+          })
+      }
+    })
+  }, delayCounter);
+    
+  // )
   // .catch(err => next(err))
 })
 
